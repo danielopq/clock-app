ef3c9044fb017253fec8207a80edde4f4b261e45
danielopq <danielopq@gmail.com>
2024-11-18 17:47:31 +0000

    quote - general layout done

--- src/components/quote/Quote.tsx
+++ src/components/quote/Quote.tsx
@@ -3,9 +3,11 @@ import './quote.css';
 const Quote:React.FC = ()=>{
     return(
         <section id="quote">
-            <div>
-                <div></div>
-                <div></div>
+            <div id="quoteSelector">
+                <p id="quoteText">
+                “The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”
+                </p>
+                <button id="quoteButton"></button>
             </div>
             <div id="author">Ada Lovelace</div>
         </section>
