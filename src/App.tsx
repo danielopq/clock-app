6829f6e111cbefddb41d8c2affa009866eeab9ce
danielopq <danielopq@gmail.com>
2024-11-19 09:38:12 +0000

    MoreInfo.tsx - created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './app.css';
 import Quote from "./components/quote/Quote";
 import Clock from "./components/clock/Clock";
+import MoreInfo from './components/moreInfo/MoreInfo';
 
 function App() {
   return (
     <main className='dayTimePic'>
       <Quote/>
       <Clock/>
+      <MoreInfo/>
     </main>
   )
 }
---
+++ src/components/moreInfo/MoreInfo.tsx
@@ -0,0 +1,8 @@
+import './moreInfo.css';
+
+const MoreInfo:React.FC = () =>{
+    return(
+        <section id="moreInfo"></section>
+    )
+}
+export default MoreInfo; 
