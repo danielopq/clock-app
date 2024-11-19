ad44306f98a7c69c9dad0e6941e67b8176cd3e1c
danielopq <danielopq@gmail.com>
2024-11-19 12:29:07 +0000

    MoreInfo.tsx - General layout done

--- src/components/moreInfo/MoreInfo.tsx
+++ src/components/moreInfo/MoreInfo.tsx
@@ -1,8 +1,32 @@
 import './moreInfo.css';
 
-const MoreInfo:React.FC = () =>{
-    return(
-        <section id="moreInfo"></section>
+const MoreInfo: React.FC = () => {
+    return (
+        <section id="moreInfo">
+            <div>
+                <div className='moreInfoContainer'>
+                    <div>
+                        <p className='moreInfoHeader'>CURRENT TIMEZONE</p>
+                        <p className='moreInfoText'>Europe/London</p>
+                    </div>
+                    <div>
+                        <p className='moreInfoHeader'>DAY OF THE YEAR</p>
+                        <p className='moreInfoText'>295</p>
+                    </div>
+                </div>
+                <div id="moreInfoSeparator"></div>
+                <div className='moreInfoContainer'>
+                    <div>
+                        <p className='moreInfoHeader'>DAY OF THE WEEK</p>
+                        <p className='moreInfoText'>5</p>
+                    </div>
+                    <div>
+                        <p className='moreInfoHeader'>WEEK NUMBER</p>
+                        <p className='moreInfoText'>45</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 export default MoreInfo; 
